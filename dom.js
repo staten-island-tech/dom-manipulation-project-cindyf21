9c059b64e058bb5df3b5ad73584aa3ae4d8746fa
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-10-13 10:14:05 -0400

    dom

---
+++ dom.js
@@ -0,0 +1,17 @@
+const DOMSelectors = {
+    form: document.querySelector("#form"), //id
+    firstName: document.querySelector(".first-name"), //class
+    h2s: document.querySelectorAll("h2"),
+    //select the textbox
+    //select ALL the h2s in one property 
+};
+// console.log(DOMSelectors.h2s);
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+    // need to add card/name object
+    DOMSelectors.h2s.forEach(
+        (el) => el.textContent = DOMSelectors.firstName.value
+    );
+});
