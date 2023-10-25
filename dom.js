6ef1300e4a2e21f8887d22212089674fe4cf8a82
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-10-25 19:01:30 -0400

    10/25

--- dom.js
+++ dom.js
@@ -1,28 +1,43 @@
 const DOMSelectors = {
-    form: document.querySelector("#form"), //id
-    firstName: document.querySelector(".first-name"), //class
+    form: document.querySelector("#form"), 
+    firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
 };
-console.log(DOMSelectors.form);
-console.log(DOMSelectors.firstName);
 console.log(DOMSelectors.h2s);
 
-
 DOMSelectors.form.addEventListener("submit", function(event){ 
-    event.preventDefault();
+event.preventDefault();
     
     const Album = makeAlbum() 
-    addcard(Album)
+    addcard(Album);
     clearFields()
-    addREmoveButtons()
-    
-    document.querySelector(".gallery")
-    document.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card"><hs class="card-title">${DOMSelectors.firstName.value}</h2></div>`
-    );
-    
+    addRemoveButtons()
+
+    document
+        .querySelector(".gallery")
+        .insertAdjacentHTML(
+        "afterbegin",
+        `<div class="card"><h2 class="card-title">${DOMSelectors.firstName.value}</h2></div>`
+    )
+
 console.log(DOMSelectors.firstName.value);
 DOMSelectors.h2s.forEach((el) => (el.textContent = DOMSelectors.firstName.value));
 });
+
+
+
+function makeAlbum() {
+
+}
+
+function addcard() {
+
+}
+
+function clearFields() {
+}
+
+function addRemoveButton() {
+
+}     
 
