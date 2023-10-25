a34ec59b5767ac41a56d16e64f408fcf6dbc284b
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-10-25 10:15:10 -0400

    dom

--- dom.js
+++ dom.js
@@ -2,24 +2,27 @@ const DOMSelectors = {
     form: document.querySelector("#form"), //id
     firstName: document.querySelector(".first-name"), //class
     h2s: document.querySelectorAll("h2"),
-    box: document.getElementById("box"),
-    button: document.getElementById("btn"),
-    //select the textbox
-    //select ALL the h2s in one property 
 };
 console.log(DOMSelectors.form);
 console.log(DOMSelectors.firstName);
 console.log(DOMSelectors.h2s);
-console.log(DOMSelectors.box);
-console.log(DOMSelectors.button);
 
-//DOMSelectors.box.insertAdjacentHTML
 
-DOMSelectors.form.addEventListener("submit", function(event){
+DOMSelectors.form.addEventListener("submit", function(event){ 
     event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    // need to add card/name object
-    DOMSelectors.h2s.forEach(
-        (el) => (el.textContent = DOMSelectors.firstName.value)
+    
+    const Album = makeAlbum() 
+    addcard(Album)
+    clearFields()
+    addREmoveButtons()
+    
+    document.querySelector(".gallery")
+    document.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card"><hs class="card-title">${DOMSelectors.firstName.value}</h2></div>`
     );
+    
+console.log(DOMSelectors.firstName.value);
+DOMSelectors.h2s.forEach((el) => (el.textContent = DOMSelectors.firstName.value));
 });
+
