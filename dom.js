a03c2fb410c08a411453e2916517ff99fab7ccce
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2023-10-26 10:14:50 -0400

    10/26

--- dom.js
+++ dom.js
@@ -8,16 +8,24 @@ console.log(DOMSelectors.h2s);
 DOMSelectors.form.addEventListener("submit", function(event){ 
 event.preventDefault();
     
-    const Album = makeAlbum() 
-    addcard(Album);
-    clearFields()
-    addRemoveButtons()
+    //const Album = makeAlbum() 
+    //addcard(Album);
+    //clearFields()
+    //addRemoveButtons()
 
     document
         .querySelector(".gallery")
         .insertAdjacentHTML(
-        "afterbegin",
-        `<div class="card"><h2 class="card-title">${DOMSelectors.firstName.value}</h2></div>`
+        //"afterend", 
+        "beforebegin",
+        //"afterbegin",
+
+      //`<div class="card"><h2 class="card-title">${DOMSelectors.firstName.value}</h2></div>`
+        `<div class="gallery" id="box">
+        <h2 class="card-title">${DOMSelectors.firstName.value}</h2>
+        <img class="image" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="">
+        <button class="button">Remove</button>
+        </div>`
     )
 
 console.log(DOMSelectors.firstName.value);
@@ -35,6 +43,7 @@ function addcard() {
 }
 
 function clearFields() {
+
 }
 
 function addRemoveButton() {
